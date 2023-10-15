7dbd26f0805c8aac5da4f6899990381c19f2e075
kasuni23 <kasunimaheshika2325@gmail.com>
2023-10-15 09:23:37 +0530

    get and push

--- helloworld/src/main/java/com/ijse/helloworld/controller/HelloController.java
+++ helloworld/src/main/java/com/ijse/helloworld/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.ijse.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,18 @@ public class HelloController {
         return "hello world";
     }
 
+    @PostMapping("/hello")
+    public String postHelloWorld() {
+        return "post request to hello world";
+    }
+
     @GetMapping("/read")
     public String readSomthing() {
         return " Reading an returning response";
+    }
+
+    @PostMapping("/read")
+    public String postReadSomething() {
+        return "Posting an returning response ";
     }
 }
